7a5fd8f1bd85cce55f66d2532af9b983dfeb8433
jackykkk <41138304+jackykwan@users.noreply.github.com>
2018-07-13 17:49:24 +0800

    button update

---
+++ Minigame/Assets/Script/Game Setting/bar control/btnControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnControl : MonoBehaviour {
+
+
+
+    public GameObject bar;
+
+    void leftbutton()
+    {
+        bar.transform.Rotate(0, 0, 1f);
+    }
+
+    void rightbutton()
+    {
+        bar.transform.Rotate(0, 0, 1f);
+    }
+
+}
