a49863db5926c27be43cc2b0f31da2acc472a30f
GitHubUserHere270 <31693527+GitHubUserHere270@users.noreply.github.com>
2018-07-13 19:13:17 +0800

    testing
    testing script for left and right turn

---
+++ Minigame/Assets/Script/Game Setting/testScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class testScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+    public bool buttonPressed;
+
+    public GameObject coin;
+
+
+    int k = 1;
+
+    public void OnPointerDown(PointerEventData eventData) {
+        if (this.name == "leftBtn") {
+            k = 1;
+        } else if (this.name == "rightBtn") {
+            k = -1;
+        }
+        buttonPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        buttonPressed = false;
+       
+    }
+
+
+    void Update() {
+
+        if (buttonPressed) {
+
+            coin.transform.position += new Vector3(0.01f*k, 0f, 0f);
+        }
+
+    }
+
+
+    
+    
+}
